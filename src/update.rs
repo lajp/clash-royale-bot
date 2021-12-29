351395d46c8efe809f36bba70ab0632329af2e33
Luukas Pörtfors <lajp@iki.fi>
2021-12-29 15:18:51 +0200

    Use lazy_static for boring chests vector

--- src/update.rs
+++ src/update.rs
@@ -4,11 +4,14 @@ use serenity::http::Http;
 use crate::clashroyale::api::*;
 use tracing::info;
 
+lazy_static::lazy_static! {
+    static ref BORING_CHESTS: Vec<&'static str> = vec!["Silver Chest", "Golden Chest", "Gold Crate", "Plentiful Gold Crate"];
+}
+
 
 pub async fn update_everything(http: &Http, db: Database) -> Result<(), anyhow::Error> {
     info!("Starting full-update");
     let startime = std::time::Instant::now();
-    let boring_chests = vec!["Silver Chest", "Golden Chest", "Gold Crate", "Plentiful Gold Crate"];
     let notfication_channel_id = env::var("NOTIFICATION_CHANNEL_ID").expect("NOTIFICATION_CHANNEL_ID not defined in .env")
         .parse::<u64>().expect("Invalid NOTIFICATION_CHANNEL_ID");
     let notfication_channel = http.get_channel(notfication_channel_id).await?;
@@ -27,7 +30,7 @@ pub async fn update_everything(http: &Http, db: Database) -> Result<(), anyhow::
             }).await?;
         }
         let mut chests = get_player_chests(&player.clash_royale_id).await?;
-        chests.retain(|c| c.index < 10 && !boring_chests.contains(&c.name.as_str()));
+        chests.retain(|c| c.index < 10 && !BORING_CHESTS.contains(&c.name.as_str()));
         for chest in chests {
             notfication_channel.id().send_message(http, |m| {
                 m.content(format!("<@{}> Tulet saamaan chestin \"{}\" {} chestin päästä!!!!", player.discord_id, chest.name, chest.index))
